c93e02d18605e74c21e6d1ffabc13c59934826f8
Eric Tsai <eric492718@gmail.com>
2025-05-08 15:00:17 +0800

    Add item to registry

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import * as React from "react"
-import { OpenInV0Button } from "@/components/open-in-v0-button"
-import { HelloWorld } from "@/registry/new-york/blocks/hello-world/hello-world"
-import { ExampleForm } from "@/registry/new-york/blocks/example-form/example-form"
-import PokemonPage from "@/registry/new-york/blocks/complex-component/page"
-import { ExampleCard } from "@/registry/new-york/blocks/example-with-css/example-card"
+import * as React from "react";
+import { OpenInV0Button } from "@/components/open-in-v0-button";
+import { HelloWorld } from "@/registry/new-york/blocks/hello-world/hello-world";
+import { ExampleForm } from "@/registry/new-york/blocks/example-form/example-form";
+import PokemonPage from "@/registry/new-york/blocks/complex-component/page";
+import { ExampleCard } from "@/registry/new-york/blocks/example-with-css/example-card";
+// For M Plus
+import LoginPage from "@/registry/m-plus/blocks/login/page";
 // This page displays items from the custom registry.
 // You are free to implement this with your own design as needed.
 
@@ -17,6 +19,18 @@ export default function Home() {
         </p>
       </header>
       <main className="flex flex-col flex-1 gap-8">
+        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
+              A simple hello world component
+            </h2>
+            <OpenInV0Button name="hello-world" className="w-fit" />
+          </div>
+          <div className="flex items-center justify-center min-h-[400px] relative">
+            <LoginPage />
+          </div>
+        </div>
+
         <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
           <div className="flex items-center justify-between">
             <h2 className="text-sm text-muted-foreground sm:pl-3">
@@ -66,5 +80,5 @@ export default function Home() {
         </div>
       </main>
     </div>
-  )
+  );
 }

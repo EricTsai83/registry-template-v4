f96e6701b7db254a8516e90fb9ef0c725356ee52
Eric Tsai <eric492718@gmail.com>
2025-06-12 13:41:12 +0800

    add chat list

--- registry/m-plus/blocks/login/page.tsx
+++ registry/m-plus/blocks/login/page.tsx
@@ -1,11 +1,9 @@
-import { LoginForm } from "@/registry/m-plus/blocks/login/components/login-form";
+import { LoginForm } from "./components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-xl flex-col gap-6 ">
-        <LoginForm />
-      </div>
+    <div className="w-full max-w-xl">
+      <LoginForm />
     </div>
   );
 }
